dec54b0bf9ae6719ef1e9fbc591e9f285501aeb7
Nícolas Lima <nicolasdelimaalves00@gmail.com>
2022-07-25 21:14:13 -0300

    att

--- Registro de Pessoas/script.js
+++ Registro de Pessoas/script.js
@@ -1,3 +1,5 @@
+const pessoas = [];
+
 let contID = 0;
 
 // Funções
@@ -7,7 +9,7 @@ function esvaziarDivAlert() {
     alert.innerHTML = "";
 };
 
-function limparInputs(inputs = []) {
+function limparInputs(...inputs) {
     inputs.forEach(input => {
         input.value = "";
     });
@@ -22,25 +24,58 @@ function criarAlerta(tipo, mensagem) {
     return alerta;
 };
 
-function criarRow(tBody, contID, nome, sobrenome, idade) {
-    const tr = document.createElement("tr");
+function mostrarRegistros(TBody) {
+    pessoas.forEach(pessoa => {
+        const tr = document.createElement("tr");
+        tr.id = `pessoa-${pessoa.id}`;
 
-    const tdContID = document.createElement("td");
-    const tdNome = document.createElement("td");
-    const tdSobrenome = document.createElement("td");
-    const tdIdade = document.createElement("td");
+        const atributos = Object.keys(pessoa);
 
-    tdContID.innerText = contID;
-    tdNome.innerText = nome;
-    tdSobrenome.innerText = sobrenome;
-    tdIdade.innerText = idade;
+        atributos.forEach(atributo => {
+            const td = document.createElement("td");
+            td.innerText = pessoa[atributo];
 
-    tr.appendChild(tdContID);
-    tr.appendChild(tdNome);
-    tr.appendChild(tdSobrenome);
-    tr.appendChild(tdIdade);
+            tr.appendChild(td);
+        });
 
-    tBody.appendChild(tr);
+        // Ação de excluir
+
+        const tdAcoes = document.createElement("td");
+
+        tdAcoes.innerHTML = `
+        <button type="button" class="btn border-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash link-danger" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path></svg>
+        </button>`;
+
+        const buttonExcluir = tdAcoes.querySelector("button");
+
+        buttonExcluir.addEventListener("click", () => {
+            deletarRegistro(TBody, pessoa.id);
+        });
+
+        tr.appendChild(tdAcoes);
+
+        TBody.appendChild(tr);
+    });
+};
+
+function deletarRegistro(TBody, id) {
+    pessoas.forEach(pessoa => {
+        if (pessoa.id == id) {
+            const index = pessoas.indexOf(pessoa);
+            
+            pessoas.splice(index, 1);
+        };
+    });
+
+    //// ARRUMAR //////////
+    trRegistro = TBody.querySelector(`tr#pessoa-${id}`);
+    TBody.removeChild(trRegistro);
+
+    if(!pessoas.length) {
+        document.querySelector("table#registrosPessoas").classList.add("d-none");
+    }
 };
 
 // Titulos
@@ -67,9 +102,7 @@ registrarPessoa.addEventListener("submit", (e) => {
     const idadeValida = !!(idade.value);
 
     if (nomeValido && sobrenomeValido && idadeValida) {
-        const TBodyRegistrosPessoas = document.querySelector("#registrosPessoas > tbody");
-
-        criarRow(TBodyRegistrosPessoas, contID, nome.value, sobrenome.value, idade.value);
+        pessoas.push({id: contID, nome: nome.value, sobrenome: sobrenome.value, idade: idade.value});
 
         contID++;
 
@@ -77,7 +110,7 @@ registrarPessoa.addEventListener("submit", (e) => {
 
         alert.appendChild(mensagemSucesso);
 
-        limparInputs([nome, sobrenome, idade]);
+        limparInputs(nome, sobrenome, idade);
     }
 
     else {
@@ -91,54 +124,57 @@ registrarPessoa.addEventListener("submit", (e) => {
 
 const registrosPessoas = document.querySelector("#registrosPessoas");
 
-// --------------------------------- INSERIR --------------------------------- \\
-
-
-
 // Badge (Nav pill)
 
 const registrarERegistros = document.querySelector("#registrarERegistros");
-
-const navPillsButtons = registrarERegistros.querySelectorAll("li > a");
 
 const registrarButton = registrarERegistros.querySelector("#registrar");
 
 const registrosButton = registrarERegistros.querySelector("#registros");
 
-navPillsButtons.forEach(button => {
-    button.addEventListener("click", () => {
+registrarButton.addEventListener("click", () => {
+    if (!registrarButton.classList.contains("active")) {
+        esvaziarDivAlert();
+
+        registrosButton.classList.toggle("active");
+        registrarButton.classList.toggle("active");
+
+        tituloRegistros.classList.add("d-none");
+        registrosPessoas.classList.add("d-none");
+
+        tituloRegistrar.classList.toggle("d-none");
+        registrarPessoa.classList.toggle("d-none");
+    };
+})
+
+registrosButton.addEventListener("click", () => {
+    if (!registrosButton.classList.contains("active")) {
         esvaziarDivAlert();
 
         registrarButton.classList.toggle("active");
         registrosButton.classList.toggle("active");
-    
+
         tituloRegistrar.classList.toggle("d-none");
-    
         registrarPessoa.classList.toggle("d-none");
-      
-        if (button.id == "registros") {
-            const registrosTBody = registrosPessoas.querySelector("table > tbody");
 
-            const temRegistro = registrosTBody.rows.length > 0;
+        const TBodyRegistrosPessoas = registrosPessoas.querySelector("tbody");
 
-            if (temRegistro) {
-                tituloRegistros.classList.toggle("d-none");
-                registrosPessoas.classList.toggle("d-none"); 
-            }
+        if (!!pessoas.length) {
+            registrosPessoas.classList.toggle("d-none");
 
-            else {
-                const alert = document.querySelector("#alert");
-                
-                const mensagemErro = criarAlerta("alert-danger", "Nenhum registro encontrado!");
+            mostrarRegistros(TBodyRegistrosPessoas);
 
-                alert.appendChild(mensagemErro);
+            registrarButton.addEventListener("click", () => {
+                TBodyRegistrosPessoas.innerHTML = "";
+            });
+        }
 
-                registrarButton.addEventListener("click", () => {
-                    tituloRegistros.classList.add("d-none");
-                    registrosPessoas.classList.add("d-none"); 
-                });
-            };
+        else {
+            const alert = document.querySelector("#alert");
+            
+            const mensagemErro = criarAlerta("alert-danger", "Nenhum registro encontrado!");
+    
+            alert.appendChild(mensagemErro);
         };
-    });
+    };
 });
-
